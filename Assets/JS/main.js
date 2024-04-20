28383e308a6c7e6ebaaa7c5ffa3576a640209e56
Ali Ahmadyari <ali.ahmadyari2002@gmail.com>
2024-04-21 00:19:47 +0330

    js cards

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,14 +1,47 @@
 const secElem = document.querySelector("section")
-let users = [
-    {title: "".title, userid: "".id, text: "".body}
-]
-async function getposts(){
-    let res = await fetch("https://jsonplaceholder.typicode.com/posts")
-    let posts = await res.json()
-    posts.splice(15)
-    console.log(posts);
-    posts.forEach(function(){
-        secElem.append()
-    })
+const mainElem = document.querySelector("main")
+const articleElem = document.querySelector("article")
+async function getPosts(){
+    const res = await fetch ("https://jsonplaceholder.typicode.com/posts")
+    const posts = await res.json()
+    return posts.slice(0, 12)
 }
-getposts()
+
+const addPostCards = async () => {
+    const posts = await getPosts();
+    posts.forEach(post =>{
+        secElem.innerHTML += (`
+        <div class="img-1">
+        <img src="./ASSETS/IMG/user-icon.png" width="100%" alt="">
+        <div class="text-7">${post.title}</div>
+        <div class="text-8">It is a long established fact that a reader will be distracted by the readable
+        content of a page when looking at its layout. The point of using Lorem Ipsum is that....</div>
+        <div class="text-9">May 20th 2020<a class="text-10" href="detail.html?id=${post.id}">Read more</a></div>
+        </div>
+        `)
+        mainElem.innerHTML = (`
+        <div class="main-1">
+        <div class="text-3">${post.title}</div>
+        <div class="text-4">It is a long established fact that a reader will be distracted by the readable
+        content of a page when looking at its layout. The point of using Lorem Ipsum is that....</div>
+        <div class="text-5">May 20th 2020<a class="text-6" href="detail.html?id=${post.id}">Read more</a></div>
+        </div>
+        <div class="main-2">
+        <img src="./Assets/IMG/image 1.png" alt="">
+        </div>
+        `)
+        articleElem.innerHTML = (`
+        <div>
+        <div class="text-11">${post.title}</div>
+        <div class="text-12">It is a long established fact that a reader will be distracted by the readable 
+        content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a 
+        more-or-less normal distribution...</div>
+        <div class="text-13">May 20th 2020<a class="text-14" href="detail.html?id=${post.id}">Read more</a></div>
+        </div>
+        <div>
+        <img src="./Assets/IMG/image 7.png" alt="">
+        </div>
+        `)
+    });
+}
+addPostCards()
